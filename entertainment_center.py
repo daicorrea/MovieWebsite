13b6824dbf1215bd4937f19b94fe5ffdd235fb64
Daiane Correa <daoli@br.ibm.com>
2018-01-20 20:11:04 -0200

    Showing code in a web page

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,23 @@
 import media
+import fresh_tomatoes
 
 harry_potter = media.Movie('Harry Potter', 'The boy who lived',
                            'https://www.harrypottermovieposters.com/wp-content/uploads/2014/05/harry-potter-and-the-deathly-hallows-part-i-movie-poster-2010-1020560181.jpg',
                            'https://www.youtube.com/watch?v=L8-e_VdwAME')
-print(harry_potter.title)
+
 avatar = media.Movie('Avatar', 'Blue Tall People',
                      "http://4.bp.blogspot.com/_1qPLMlz01yQ/SwTA3GFVzxI/AAAAAAAADts/irJykYj7hJs/s1600/MarketSaw_03+Nov.+18+23.51.jpg",
                      'https://www.youtube.com/watch?v=d1_JBMrrYw8')
-print(avatar.storyline)
-avatar.show_trailer()
+
+hunger_games = media.Movie('Hunger Games', 'A really real reality show',
+                           'https://vignette.wikia.nocookie.net/thehungergames/images/c/ca/Hunger-Games-Movie-Poster-Jennifer-Lawrence.jpg/revision/latest?cb=20110417161424',
+                           'https://www.youtube.com/watch?v=FovFG3N_RSU')
+
+a_monster_call = media.Movie('A Monster Calls', 'Slap in your face... If you get it...',
+                             'https://www.woodbangersentertainment.com/wp-content/uploads/2017/06/vdHUj9cyRe7fIYdJFMyc7elnawC.jpg',
+                             'https://www.youtube.com/watch?v=R2Xbo-irtBA')
+
+movies = [harry_potter, avatar, hunger_games, a_monster_call]
+
+fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -4,8 +4,8 @@ class Movie():
     def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer_link):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.poster_image = movie_poster_image
-        self.trailer_link = movie_trailer_link
+        self.poster_image_url = movie_poster_image
+        self.trailer_youtube_url = movie_trailer_link
 
     def show_trailer(self):
-        webbrowser.get('safari').open_new(self.trailer_link)
+        webbrowser.get('safari').open_new(self.trailer_youtube_url)

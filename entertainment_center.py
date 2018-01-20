a7a03f8179d87cbae4f4bb5c28d8b146d9a653cc
Daiane Correa <daoli@br.ibm.com>
2018-01-20 18:46:50 -0200

    Adding Another Movie

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,9 @@
 import media
 
-harry_potter = media.Movie('Harry Potter', 'The boy who lived', 'https://www.harrypottermovieposters.com/wp-content/uploads/2014/05/harry-potter-and-the-deathly-hallows-part-i-movie-poster-2010-1020560181.jpg', 'https://www.youtube.com/watch?v=L8-e_VdwAME')
+harry_potter = media.Movie('Harry Potter', 'The boy who lived',
+                           'https://www.harrypottermovieposters.com/wp-content/uploads/2014/05/harry-potter-and-the-deathly-hallows-part-i-movie-poster-2010-1020560181.jpg',
+                           'https://www.youtube.com/watch?v=L8-e_VdwAME')
+print(harry_potter.title)
+avatar = media.Movie('Avatar', 'Blue Tall People', "Poster link", 'Youtube Link')
+print(avatar.storyline)
 
-print (harry_potter.title)

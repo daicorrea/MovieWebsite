887af1534e6c22fd0f282b1cfeb817800139e768
Daiane Correa <daoli@br.ibm.com>
2018-01-20 19:36:04 -0200

    Creating Show Trailer Function

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,6 +4,9 @@ harry_potter = media.Movie('Harry Potter', 'The boy who lived',
                            'https://www.harrypottermovieposters.com/wp-content/uploads/2014/05/harry-potter-and-the-deathly-hallows-part-i-movie-poster-2010-1020560181.jpg',
                            'https://www.youtube.com/watch?v=L8-e_VdwAME')
 print(harry_potter.title)
-avatar = media.Movie('Avatar', 'Blue Tall People', "Poster link", 'Youtube Link')
+avatar = media.Movie('Avatar', 'Blue Tall People',
+                     "http://4.bp.blogspot.com/_1qPLMlz01yQ/SwTA3GFVzxI/AAAAAAAADts/irJykYj7hJs/s1600/MarketSaw_03+Nov.+18+23.51.jpg",
+                     'https://www.youtube.com/watch?v=d1_JBMrrYw8')
 print(avatar.storyline)
+avatar.show_trailer()
 
--- media.py
+++ media.py
@@ -1,6 +1,11 @@
+import webbrowser
+
 class Movie():
-   def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer_link):
-       self.title = movie_title
-       self.storyline = movie_storyline
-       self.poster_image = movie_poster_image
-       self.trailer_link = movie_trailer_link
+    def __init__(self, movie_title, movie_storyline, movie_poster_image, movie_trailer_link):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image = movie_poster_image
+        self.trailer_link = movie_trailer_link
+
+    def show_trailer(self):
+        webbrowser.get('safari').open_new(self.trailer_link)
